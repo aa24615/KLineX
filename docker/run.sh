6f79bf21bee3d97a7ceaf696a0496f488dbfbcde
zyan <zyan@php127.com>
2025-04-24 20:25:56 +0800

    新增实时分析

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,10 @@
 php artisan migrate
 #php artisan update:tables
 #php artisan queue:work &
-php artisan schedule:work &
+php artisan schedule:work
 
 php artisan serve --port=8018 --host=0.0.0.0
+
+#php composer.phar install
+
+#sleep 10000;
