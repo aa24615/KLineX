cb7effcf1be95be74da212652f2b11bb33bd3fec
zyan <zyan@php127.com>
2025-04-25 07:47:22 +0800

    支持 between 搜索

--- app/Common/Lists/ListsSearchTrait.php
+++ app/Common/Lists/ListsSearchTrait.php
@@ -2,6 +2,8 @@
 
 namespace App\Common\Lists;
 
+use Illuminate\Support\Facades\DB;
+
 trait ListsSearchTrait
 {
 
@@ -70,11 +72,28 @@ trait ListsSearchTrait
                     }
                     $where[] = [$whereFields, 'between', [$this->startTime, $this->endTime]];
                     break;
-                case 'between': // todo Laravel不支持
-                    if (empty($this->start) || empty($this->end)) {
-                        break;
+                case 'between': // Laravel 已支持 以,号隔开
+
+                    foreach ($whereFields as $whereField) {
+                        $paramsName = substr_symbol_behind($whereField);
+                        if (!isset($this->params[$paramsName]) || empty($this->params[$paramsName])) {
+                            continue;
+                        }
+
+                        $values = explode(',',$this->params[$paramsName]);
+
+                        if(count($values) >= 2){
+                            $this->start = trim($values[0]);
+                            $this->end = trim($values[1]);
+//                            $where[] = [$paramsName, 'between', "{$this->start} and {$this->end}"];
+//                            $where[] = [DB::raw("{$paramsName} between {$this->start} and {$this->end}")];
+                            $where[] = [$paramsName, '>=', $this->start];
+                            $where[] = [$paramsName, '<=', $this->end];
+                        }else{
+                            $where[] = [$paramsName, '=', $this->params[$paramsName]];
+                        }
                     }
-                    $where[] = [$whereFields, 'between', [$this->start, $this->end]];
+
                     break;
                 case 'find_in_set': // todo Laravel不支持
                     foreach ($whereFields as $whereField) {
