fabb68e9efbc1a2a2d22c1b28aaf3b51ce1510ec
zyan <zyan@php127.com>
2025-05-09 23:45:02 +0800

    新增通知模块

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
+rm -rf /www/storage/app/private/stock/
+
 php artisan db:clear_invalid_data
 php artisan backup:run
 
+rm -rf /www/database/install/
+mkdir -p /www/database/install/
 mv /www/storage/app/private/stock/ /www/database/install/
